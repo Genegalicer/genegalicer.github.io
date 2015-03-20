157837abb85e45002236ed6939fb0c332aa5c0f3
Genegalicer <genegalicer@gmail.com>
2015-03-19 17:17:47 -0700

    flappy

---
+++ gist/clicks.js
@@ -0,0 +1,13 @@
+/*global $*/
+/*jslint sloppy:true, browser: true, white: true*/
+
+$("h1.page-title").text("My code runs!");
+
+$(window).on('click', function (e) {
+    $('img.logo').css({left: e.pageX,
+                       top: e.pageY});
+    
+    $('.logo').toggleClass('rotated');
+});
+
+
